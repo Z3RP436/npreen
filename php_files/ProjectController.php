e88021c32d8da00c4c9aa8b2f6b87f95a82dd0cd
Test User <test@mail.com>
2019-05-17 11:55:16 +0200

    uploaded the website on github

--- php_files/ProjectController.php
+++ php_files/ProjectController.php
@@ -12,7 +12,7 @@ require_once(__DIR__.'/Project.php');
                 ++$index => new GithubProject("Chart-Libary","A libary to create small Line-Charts.","z3rp436","chart"),
                 ++$index => new GithubProject("Standford-Dogs","A Neural Network project that is using my own libary to recognize dogs in a picture. I'm a stanford database with round 100.000 pictures of dogs.","z3rp436","stanfordDogs"),
                 ++$index => new GithubProject("HKI","A school project where we tried to build up a neural netork that recognize handwritten digits.","z3rp436","hki"),
-                ++$index => new Project("Website","This website is currently a Project")
+                ++$index => new GithubProject("Website","This website is currently a Project","z3rp436","npreen")
             ];
         }
 
